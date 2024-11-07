f7db0f77fecebba25fae82e28f2a9036bcca11d2
Schkimansky <ahmadchawla1432@gmail.com>
2024-11-07 17:35:54 +0500

    Update odoo16 files

--- gts_whatsapp_pos/static/src/js/receipt.js
+++ gts_whatsapp_pos/static/src/js/receipt.js
@@ -1,17 +1,81 @@
-/** @odoo-module */
-import { ReceiptScreen } from "@point_of_sale/app/screens/receipt_screen/receipt_screen";
-import { OrderReceipt } from "@point_of_sale/app/screens/receipt_screen/receipt/order_receipt";
-import { _t } from "@web/core/l10n/translation";
-import { patch } from "@web/core/utils/patch";
-import { useState } from "@odoo/owl";
+odoo.define('gts_whatsapp_pos.ReceiptScreen', function(require) {
+	"use strict";
 
-patch(ReceiptScreen.prototype, {
-    setup() {
-        super.setup(...arguments);
+    const { Printer } = require('point_of_sale.Printer');
+	const ReceiptScreen = require('point_of_sale.ReceiptScreen');
+    const Registries = require('point_of_sale.Registries');
 
-        const partner = this.currentOrder.get_partner();
-        // console.log(Object.getOwnPropertyNames(this.currentOrder).concat(Object.getOwnPropertyNames(Object.getPrototypeOf(this.currentOrder))));
-        const orderName = this.currentOrder.get_name();
+    const { onMounted, useRef, status } = owl;
+
+    const WhatsappReceiptScreen = ReceiptScreen =>
+        class extends ReceiptScreen {
+            constructor() {
+                super(...arguments);
+                const partner = this.currentOrder.get_partner();
+                const orderName = this.currentOrder.get_name();
+                var number = "";
+
+                if (partner != null) {
+                    number = partner.phone || "";
+                }
+
+                this.inputWhatsapp = number
+                this.inputMessage = `Hello, Here is your receipt for the following order id: ${orderName}.`
+                this.isSending = false
+                this.whatsappButtonDisabled = true
+                
+            }
+            is_valid_mobile() {
+                const value = this.inputWhatsapp;
+                if (value) {
+                    const valueLen = value.replace(/[^0-9]/g, "").length;
+                    return valueLen > 8 && valueLen < 15;
+                }
+                return false;
+            }
+
+            onInputWhatsapp(ev) {
+                this.whatsappButtonDisabled = false;
+                this.inputWhatsapp = ev.target.value;
+            }
+
+            async onSendWhatsapp() {
+               if (this.isSending) {
+                   return;
+               }
+               this.isSending = true;
+
+               setTimeout(async () => {
+                   try {
+                       await this._sendWhatsappToCustomer();
+                   } catch (error) {
+                       console.error(error);
+                       this.whatsappButtonDisabled = true;
+                   }
+
+                   this.isSending = false;
+               }, 100);
+            }
+
+            async _sendWhatsappToCustomer() {
+                var number = this.inputWhatsapp;
+                var message = this.inputMessage;
+
+                const receiptString = this.orderReceipt.el.innerHTML;
+                const printer = new Printer(null, this.env.pos);
+                const ticketImage = await printer.htmlToImg(receiptString);
+
+                await this.rpc({
+                    model: 'pos.order',
+                    method: 'whatsapp_template_message',
+                    args: [number, message, ticketImage],
+                });
+            }
+		};
+
+	Registries.Component.extend(ReceiptScreen, WhatsappReceiptScreen);
+	return ReceiptScreen;
+});
         var number = "";
 
         if (partner != null) {
@@ -66,14 +130,5 @@ patch(ReceiptScreen.prototype, {
         var number = this.orderUiState.inputWhatsapp;
         var message = this.orderUiState.inputMessage;
 
-        const ticketImage = await this.renderer.toJpeg(
-            OrderReceipt,
-            {
-                data: this.pos.get_order().export_for_printing(),
-                formatCurrency: this.env.utils.formatCurrency,
-            },
-            { addClass: "pos-receipt-print" }
-        );
-        await this.orm.call("pos.order", "whatsapp_template_message", [number, message, ticketImage]);
     },
 });
